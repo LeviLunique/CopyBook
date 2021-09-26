7270b99526ba5647cca33deae2f64ed69be7c57c
Levi Lunique <levi.lunique@gmail.com>
2021-09-26 15:45:23 -0400

    Post

---
+++ CopyBook/Model/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  CopyBook
+//
+//  Created by user204006 on 9/26/21.
+//
+
+import Foundation
+struct Post: Codable, Identifiable {
+    let userId, id: Int
+    let title, body: String
+}
--- CopyBook/ViewController/FeedTableViewController.swift
+++ CopyBook/ViewController/FeedTableViewController.swift
@@ -8,16 +8,43 @@
 import UIKit
 
 class FeedTableViewController: UITableViewController {
-
+    
+    private let kBaseURL = "https://jsonplaceholder.typicode.com"
+    
+    private var posts = [Post](){
+        didSet{
+            tableView.reloadData()
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         FeedTableViewCell.register(in: tableView)
         
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let url = URL(string: "\(kBaseURL)/posts"){
+            let session = URLSession(
+                configuration: URLSessionConfiguration.default,
+                delegate: self,
+                delegateQueue: OperationQueue.main)
+            
+            session.dataTask(with: url).resume()
+        }
+        
+    }
     
     override func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return posts.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -25,8 +52,23 @@ class FeedTableViewController: UITableViewController {
         
         cell.setup(with: nil)
         
+        let post = posts[indexPath.row]
+        
+        cell.postText?.text = post.body
         
         return cell
     }
+}
 
+
+extension FeedTableViewController: URLSessionDataDelegate{
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        if let response = dataTask.response as? HTTPURLResponse,
+           response.statusCode >= 200, response.statusCode < 300 {
+            if let posts = try? JSONDecoder().decode([Post].self, from: data){
+                self.posts = posts
+            }
+            
+        }
+    }
 }
